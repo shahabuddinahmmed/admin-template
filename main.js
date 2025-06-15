3eaaa6f4a7e01c3029de815bbc5cda15395557fd
shahabuddin-ahammed <shahabuddin.ice@gmail.com>
2025-06-15 12:58:03 +0600

    Initiall

---
+++ main.js
@@ -0,0 +1,290 @@
+// SIDEBAR DROPDOWN
+const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+const sidebar = document.getElementById('sidebar');
+
+allDropdown.forEach(item => {
+    const a = item.parentElement.querySelector('a:first-child');
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if (!this.classList.contains('active')) {
+            allDropdown.forEach(i => {
+                const aLink = i.parentElement.querySelector('a:first-child');
+
+                aLink.classList.remove('active');
+                i.classList.remove('show');
+            })
+        }
+
+        this.classList.toggle('active');
+        item.classList.toggle('show');
+    });
+});
+
+
+//SIDEBAR COLLAPSE
+const toggleSidebar = document.querySelector('nav .toggle-sidebar');
+const allSideDivider = document.querySelectorAll('#sidebar .divider');
+
+if (sidebar.classList.contains('hide')) {
+    allSideDivider.forEach(item => {
+        item.textContent = '-';
+    })
+    allDropdown.forEach(item => {
+        const a = item.parentElement.querySelector('a:first-child');
+        a.classList.remove('active');
+        item.classList.remove('show');
+    })
+} else {
+    allSideDivider.forEach(item => {
+        item.textContent = item.dataset.text;
+    })
+}
+
+toggleSidebar.addEventListener('click', () => {
+    sidebar.classList.toggle('hide');
+
+    if (sidebar.classList.contains('hide')) {
+        allSideDivider.forEach(item => {
+            item.textContent = '-';
+        })
+        allDropdown.forEach(item => {
+            const a = item.parentElement.querySelector('a:first-child');
+            a.classList.remove('active');
+            item.classList.remove('show');
+        })
+    } else {
+        allSideDivider.forEach(item => {
+            item.textContent = item.dataset.text;
+        })
+    }
+})
+
+sidebar.addEventListener('mouseleave', () => {
+    if (sidebar.classList.contains('hide')) {
+        allDropdown.forEach(item => {
+            const a = item.parentElement.querySelector('a:first-child');
+            a.classList.remove('active');
+            item.classList.remove('show');
+        });
+        allSideDivider.forEach(item => {
+            item.textContent = '-';
+        })
+    }
+});
+
+sidebar.addEventListener('mouseenter', () => {
+    if (sidebar.classList.contains('hide')) {
+        allDropdown.forEach(item => {
+            const a = item.parentElement.querySelector('a:first-child');
+            a.classList.remove('active');
+            item.classList.remove('show');
+        });
+        allSideDivider.forEach(item => {
+            item.textContent = item.dataset.text;
+        })
+    }
+});
+
+// PROFILE DROPDOWN
+const profile = document.querySelector('nav .profile');
+const imgProfile = profile.querySelector('img');
+const dropdownProfile = profile.querySelector('.profile-link');
+
+imgProfile.addEventListener('click', () => {
+    dropdownProfile.classList.toggle('show');
+});
+
+// MENU
+const allMenu = document.querySelectorAll('main .content-data .head .menu');
+
+allMenu.forEach(item => {
+    const icon = item.querySelector('.icon');
+    const menuLink = item.querySelector('.menu-link');
+
+    icon.addEventListener('click', () => {
+        menuLink.classList.toggle('show');
+    })
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target !== imgProfile) {
+        if (e.target !== dropdownProfile) {
+            if (dropdownProfile.classList.contains('show')) {
+                dropdownProfile.classList.remove('show');
+            }
+        }
+    }
+    allMenu.forEach(item => {
+        const icon = item.querySelector('.icon');
+        const menuLink = item.querySelector('.menu-link');
+
+        if (e.target !== icon) {
+            if (e.target !== menuLink) {
+                if (menuLink.classList.contains('show')) {
+                    menuLink.classList.remove('show');
+                }
+            }
+        }
+    })
+});
+
+// PROGRESSBAR
+const allProgress = document.querySelectorAll('main .card .progress');
+
+allProgress.forEach(item => {
+    item.style.setProperty('--value', item.dataset.value);
+})
+
+
+
+// APEXCHARTS 
+var options = {
+    series: [{
+        name: 'series1',
+        data: [31, 40, 28, 51, 42, 109, 100]
+    }, {
+        name: 'series2',
+        data: [11, 32, 45, 32, 34, 52, 41]
+    }],
+    chart: {
+        height: 350,
+        type: 'area'
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'smooth'
+    },
+    xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+    },
+    tooltip: {
+        x: {
+            format: 'dd/MM/yy HH:mm'
+        },
+    },
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+//APEXCHART PIE
+var options = {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+        width: 380,
+        type: 'pie',
+    },
+    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart-pie"), options);
+chart.render();
+
+//APEXCHART Stacked Columns
+var options = {
+    series: [{
+        name: 'PRODUCT A',
+        data: [44, 55, 41, 67, 22, 43]
+    }, {
+        name: 'PRODUCT B',
+        data: [13, 23, 20, 8, 13, 27]
+    }, {
+        name: 'PRODUCT C',
+        data: [11, 17, 15, 15, 21, 14]
+    }, {
+        name: 'PRODUCT D',
+        data: [21, 7, 25, 13, 22, 8]
+    }],
+    chart: {
+        type: 'bar',
+        height: 350,
+        stacked: true,
+        toolbar: {
+            show: true
+        },
+        zoom: {
+            enabled: true
+        }
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+            }
+        }
+    }],
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            borderRadius: 10
+        },
+    },
+    xaxis: {
+        type: 'datetime',
+        categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+            '01/05/2011 GMT', '01/06/2011 GMT'
+        ],
+    },
+    legend: {
+        position: 'right',
+        offsetY: 40
+    },
+    fill: {
+        opacity: 1
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart-stacked-columns"), options);
+chart.render();
+
+//APEXCHART Semi Donut
+var options = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  plotOptions: {
+    pie: {
+      startAngle: -90,
+      endAngle: 90,
+      offsetY: 10
+    }
+  },
+  grid: {
+    padding: {
+      bottom: -80
+    }
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart-semi-donut"), options);
+  chart.render();
